342937f04b60f99d41d64b7c7f9e1f6a79554fc1
Dmitry Tsvetkov <tsvetkov@tn.ru>
2023-03-27 22:38:28 +0300

    ping

--- internal/server/repository/db/db.go
+++ internal/server/repository/db/db.go
@@ -10,64 +10,56 @@ import (
 )
 
 type DBStor struct {
-	Config config.Config
+	config config.Config
 	db     *sql.DB
 }
 
-func (D DBStor) Ping() error {
+func New(cfg config.Config) DBStor {
+	db, err := sql.Open("pgx", cfg.DSN)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	return DBStor{
+		config: cfg,
+		db:     db,
+	}
+}
+
+func (d DBStor) Ping() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
 	defer cancel()
-	if err := D.db.PingContext(ctx); err != nil {
+	if err := d.db.PingContext(ctx); err != nil {
 		panic(err)
 	}
 	fmt.Println("ping!")
 	return nil
 }
 
-func (D DBStor) GetAll() (map[string]float64, map[string]int64) {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) GetAll() (map[string]float64, map[string]int64) {
+	return nil, nil
 }
 
-func (D DBStor) Get(typ, name string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) Get(typ, name string) (string, error) {
+	return "", nil
 }
 
-func (D DBStor) Set(typ, name, value string) error {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) Set(typ, name, value string) error {
+	return nil
 }
 
-func (D DBStor) GetMetricsByID(id, typ string, key string) (*models.Metric, error) {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) GetMetricsByID(id, typ string, key string) (*models.Metric, error) {
+	return nil, nil
 }
 
-func (D DBStor) SetMetrics(metric *models.Metric) error {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) SetMetrics(metric *models.Metric) error {
+	return nil
 }
 
-func (D DBStor) RestoreMetrics(filename string) error {
-	//TODO implement me
-	panic("implement me")
+func (d DBStor) RestoreMetrics(filename string) error {
+	return nil
 }
 
-func (D DBStor) WriteMetrics() error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func New(cfg config.Config) *DBStor {
-	db, err := sql.Open("pgx", cfg.DSN)
-	if err != nil {
-		fmt.Println("err:", err)
-	}
-	defer db.Close()
-	return &DBStor{
-		Config: cfg,
-		db:     db,
-	}
+func (d DBStor) WriteMetrics() error {
+	return nil
 }
